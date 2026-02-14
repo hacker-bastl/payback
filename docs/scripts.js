df9467f7e0ed214707f2753419f9add1625f9ca3
fairphonebastl <hacker-bastl@users.noreply.github.com>
2026-02-15 00:01:32 +0100

    commit

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,36 +1,19 @@
-const simulator = {
-	barcode: function(command) {
-		simulator.image({
-			parameter: {
-				code: 'EAN13',
-				data: command.code,
-			},
-		})
-	},
-	image: function(command) {
-
-		const address = `//${'barcode.tec-it.com/barcode.ashx'}?${new URLSearchParams(command.parameter).toString()}`;
-		const image = document.body.querySelector('#card>img');
-		const loader = document.createElement('img');
-		loader.addEventListener('load', function() {
-			image.setAttribute('width', loader.width);
-			image.setAttribute('height', loader.height);
-			image.setAttribute('src', address);
-		});
-		loader.setAttribute('src', address);
-		loader.setAttribute('src', address);
-	},
-};
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-	simulator.barcode({
-		code: Array.from([
-			2401090558439, // demo
-			2402016060104, // demo
-			2408775194852, // aral
-		]).at(parseInt(Math.random() * 3)),
-	})
+	const code = Array.from([
+		2401090558439, // demo
+		2402016060104, // demo
+		2408775194852, // aral
+	]).at(parseInt(Math.random() * 3));
+
+	const address = `//${'barcode.tec-it.com/barcode.ashx'}?${new URLSearchParams({code: 'EAN13',data:code}).toString()}`;
+	const image = document.body.querySelector('#card>img');
+	const loader = document.createElement('img');
+	loader.addEventListener('load', function() {
+		image.setAttribute('width', loader.width);
+		image.setAttribute('height', loader.height);
+		image.setAttribute('src', address);
+	});
+	loader.setAttribute('src', address);
+	loader.setAttribute('src', address);
+
 });
