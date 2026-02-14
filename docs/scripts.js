8ca6dac77f74027827f2fda894f07d9128cf1069
fairphonebastl <hacker-bastl@users.noreply.github.com>
2026-02-14 22:16:26 +0100

    commit

--- docs/scripts.js
+++ docs/scripts.js
@@ -11,7 +11,58 @@ const simulator = {
 	},
 	qrcode: function(command) {
 		simulator.image({
+			{
+				provider: 'genqrcode.com/embedded',
+				section: '#qr-code',
+				parameter: {
 
+					text: command.code,
+
+					width: 500,
+					height: 500,
+					bordersize: 2,
+					imageformat: 'svg',
+
+					frame_text: 'SCAN ME',
+					language: 'en',
+
+					background_color: '#FFFFFFFF',
+					inner_eye_color: '#000000',
+					outer_eye_color: '#000000',
+					color: 'FF0000',
+
+					frame_text_icon_color: '#000000',
+					frame_text_icon: null,
+					frame_color: '#000000',
+					frame_background_color: '#FFFFFF',
+					frame_text_color: '#FFFFFF',
+
+					gradient_style: 0,
+					gradient_color_start: '#FF0000',
+					gradient_color_end: '#7F007F',
+					gradient_start_offset: 5,
+					gradient_end_offset: 95,
+
+					invert_colors: false,
+
+					logo_remove_background: null,
+					logo: null,
+
+					stl_type: 1,
+					stl_size: 100,
+					stl_qr_height: 1.5,
+					stl_base_height: 2,
+					stl_include_stands: false,
+					stl_qr_magnet_type: 3,
+					stl_qr_magnet_count: 0,
+
+					inner_eye_style: 0,
+					outer_eye_style: 0,
+					frame_style: 0,
+					style: 0,
+					type: 0,
+				}
+			}
 		});
 	},
 	image: function(command) {
@@ -39,79 +90,11 @@ document.addEventListener('DOMContentLoaded', function() {
 			2408775194852
 		]).at(parseInt(Math.random() * 3)),
 	})
-
 });
 
 
 document.addEventListener('DOMContentLoaded', function() {
-	dispatchEvent(new CustomEvent('qrcode', {
-		detail: '989224010905584390036164906591784883046512702842838211301612433891020016615416515525271611162339640155201571077813781111046542065626349766452802631144381802731830912061660828688061876077996438347039552679405367001711839606582546176359921246424520366665421949208817288330482',
-	}));
-});
-
-addEventListener('qrcode', function(event) {
-
-	const command = {
-		provider: 'genqrcode.com/embedded',
-		section: '#qr-code',
-		parameter: {
-
-			text: event.detail,
-
-			width: 500,
-			height: 500,
-			bordersize: 2,
-			imageformat: 'svg',
-
-			frame_text: 'SCAN ME',
-			language: 'en',
-
-			background_color: '#FFFFFFFF',
-			inner_eye_color: '#000000',
-			outer_eye_color: '#000000',
-			color: 'FF0000',
-
-			frame_text_icon_color: '#000000',
-			frame_text_icon: null,
-			frame_color: '#000000',
-			frame_background_color: '#FFFFFF',
-			frame_text_color: '#FFFFFF',
-
-			gradient_style: 0,
-			gradient_color_start: '#FF0000',
-			gradient_color_end: '#7F007F',
-			gradient_start_offset: 5,
-			gradient_end_offset: 95,
-
-			invert_colors: false,
-
-			logo_remove_background: null,
-			logo: null,
-
-			stl_type: 1,
-			stl_size: 100,
-			stl_qr_height: 1.5,
-			stl_base_height: 2,
-			stl_include_stands: false,
-			stl_qr_magnet_type: 3,
-			stl_qr_magnet_count: 0,
-
-			inner_eye_style: 0,
-			outer_eye_style: 0,
-			frame_style: 0,
-			style: 0,
-			type: 0,
-		}
-	};
-
-	const address = `//${command.provider}?${new URLSearchParams(command.parameter).toString()}`;
-	const parent = document.body.querySelector(command.section);
-	const image = document.createElement('img');
-	image.setAttribute('src', address);
-	image.addEventListener('load', function() {
-		image.setAttribute('width', image.width);
-		image.setAttribute('height', image.height);
-		parent.appendChild(image);
+	simulator.qrcode({
+		code: '989224010905584390036164906591784883046512702842838211301612433891020016615416515525271611162339640155201571077813781111046542065626349766452802631144381802731830912061660828688061876077996438347039552679405367001711839606582546176359921246424520366665421949208817288330482',
 	});
-	image.setAttribute('src', address);
 });
