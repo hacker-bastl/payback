5f96aa988c156301dbd6a48717ad293edd19dedc
fairphonebastl <hacker-bastl@users.noreply.github.com>
2026-02-15 00:15:50 +0100

    commit

--- docs/scripts.js
+++ docs/scripts.js
@@ -7,21 +7,20 @@ document.addEventListener('DOMContentLoaded', function repeat() {
 		2408775194852, // aral
 	]).at(parseInt(Math.random() * 3));
 
-	const address = `//${'barcode.tec-it.com/barcode.ashx'}?${new URLSearchParams({code:'EAN13',data:code}).toString()}`;
 	const image = document.body.querySelector('#card>img');
-	const url = new URL(`https://${image.getAttribute('src')}`);
-	url.searchParams.set('data', code)
+	const address = new URL(`https://${image.getAttribute('src')}`);
+	address.searchParams.set('data', code);
 
 	const loader = document.createElement('img');
 	loader.addEventListener('load', function() {
 		image.setAttribute('width', loader.width);
 		image.setAttribute('height', loader.height);
-		image.setAttribute('src', url.href);
+		image.setAttribute('src', address.href);
 		setTimeout(repeat, 3 * 1E3);
 	});
 	loader.addEventListener('error', function() {
 		setTimeout(repeat, 7 * 1E3);
 	});
-	loader.setAttribute('src', url.href);
+	loader.setAttribute('src', address.href);
 
 });
