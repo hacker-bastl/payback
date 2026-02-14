7ae35fb5569b2cae8f0b2bec0158e1104fc54da4
fairphonebastl <hacker-bastl@users.noreply.github.com>
2026-02-14 22:03:23 +0100

    commit

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,43 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const footer = document.body.querySelector('footer');
-	const request = new XMLHttpRequest();
-	request.addEventListener('load', function() {
-		const partner = JSON.parse(request.responseText);
-		const images = partner.sort(function() {
-			return Math.random() < 0.5 ? -1 : +1;
-		}).slice(0, 5);
-		footer.innerHTML = Array.from([
-			'<strong>Jetzt Punkte einl&ouml;sen</strong>',
-			images.map(function(url) {
-				return `<a href="//www.payback.de/partner"><img src="${url}" /></a>`;
-			}).join('\n'),
-			`<small>Du kannst <strong>${2000}&deg;P</strong> einl&ouml;sen</small>`
-		]).join('<br />');
-	});
-	request.open('GET', 'partner.json');
-	request.send(null);
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-	const header = document.body.querySelector('header');
-	const images = Array.from([
-		'//www.payback.de/resource/blob/332584/10d8a6396151a42b8387814cbe7f3129/0-payback-karte-img-resdata4.jpg',
-		'//assets.payback.de/static/5032b0a7-87c9-4f5f-80f3-f3d81277b7f7--1-head-image-resdata4.webp',
-		'//www.payback.de/resource/blob/332600/4bdc84e40ea00350c740853f78546e46/1-amex-img-resdata4.jpg',
-		'//www.payback.de/resource/blob/332566/92b1aacf22cfe333393ae94b431bb3d8/compare-visa-img-resdata4.webp',
-		'//is.payback.de/banners/408/files/00/31/b2/7d/000003256957.jpg',
-		'//assets.payback.de/static/a86d3bea-3557-4430-ab74-2366908d3bb6--13-amex-teaser-img-resdata4.jpg',
-		'//www.payback.group/fileadmin/Images/Logos/Logo_PAYBACK.svg',
-	]);
-	const address = images.at(parseInt(Math.random() * images.length));
-	header.innerHTML = `<img src="${address}" />`;
-});
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
 	dispatchEvent(new CustomEvent('barcode', {
 		detail: 2408775194852,
 	}));
